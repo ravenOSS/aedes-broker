272c1923cb5ffaa929495fcaabefac49b690e9bf
ravenOSS <35054853+ravenOSS@users.noreply.github.com>
2018-09-18 13:47:13 -0600

    commit code

---
+++ redisConfig.js
@@ -0,0 +1,11 @@
+let config = {
+  mqtt_port: 1883,
+  ws_port: 8880,
+  redis_port: 6379,
+  redis_host: '192.168.0.101',
+  redis_pass: 'carson',
+  redis_dbA: 0,
+  redis_dbB: 1
+};
+
+module.exports = config;
