6d68c4848e2d7b89db1c9fcc93a1d1a4b03487ad
David Richards <35054853+ravenOSS@users.noreply.github.com>
2022-08-14 23:13:45 -0600

    Task: move redis config to own file

--- aedesWebsocketRedis.js
+++ aedesWebsocketRedis.js
@@ -1,91 +1,85 @@
-'use strict';
-let redis = require('mqemitter-redis');
-let config = require('./redisConfig');
-/*
-let config = {
-  mqtt_port: 1883,
-  ws_port: 8880,
-  redis_port: 6379,
-  redis_host: '192.168.0.101',
-  redis_pass: 'carson',
-  redis_dbA: 0,
-  redis_dbB: 1
-};
-*/
-let mq = redis({
-  port: config.redis_port,
-  host: config.redis_host,
-  password: config.redis_pass,
-  db: config.redis_dbA,
-  family: 4
-});
+'use strict'
+const config = require('./redisConfig')
 
-let persistence = require('aedes-persistence-redis')({
-  port: config.redis_port,
-  host: config.redis_host,
-  family: 4, // 4 (IPv4) or 6 (IPv6)
-  password: config.redis_pass,
-  db: config.redis_dbA,
+const mq = require('mqemitter-redis')({
+	port: config.redis_port,
+	host: config.redis_host,
+	// password: config.redis_pass,
+	db: config.redis_dbA,
+	family: 4,
+})
 
-  maxSessionDelivery: 6000 // maximum offline messages deliverable on client CONNECT, default is 1000
-  // packetTTL: function (packet) { // offline message TTL, default is disabled
-  //  return 10; // seconds
-});
+const persistence = require('aedes-persistence-redis')({
+	port: config.redis_port,
+	host: config.redis_host,
+	family: 4, // 4 (IPv4) or 6 (IPv6)
+	// password: config.redis_pass,
+	db: config.redis_dbB,
 
-let aedesOptions = {mq, persistence, concurrency: 200};
+	maxSessionDelivery: 1000, // maximum offline messages deliverable on client CONNECT, default is 1000
+	// packetTTL: function (packet) { // offline message TTL, default is disabled
+	//  return 10; // seconds
+})
 
-let aedes = require('aedes')(aedesOptions);
-let server = require('net').createServer(aedes.handle);
-let httpServer = require('http').createServer();
-let ws = require('websocket-stream');
+const aedesOptions = { mq, persistence, concurrency: 200 }
+
+const aedes = require('aedes')(aedesOptions)
+const server = require('net').createServer(aedes.handle)
+const httpServer = require('http').createServer()
+const ws = require('websocket-stream')
+ws.createServer({ server: httpServer }, aedes.handle)
 
 server.listen(config.mqtt_port, function () {
-  console.log('MQTT server listening on port: [$config.mqtt_port]');
-});
-
-ws.createServer({ server: httpServer }, aedes.handle);
+	console.log(`MQTT server listening on port: ${config.mqtt_port}`)
+})
 
 httpServer.listen(config.ws_port, function () {
-  console.log('WS server listening on port', config.ws_port);
-});
+	console.log(`WS server listening on port: ${config.ws_port}`)
+})
 
 // logging({
 //   aedes: aedes,
 //   servers: servers
 // });
 
-aedes.on('client', client => {
-  console.log(`Client [${client.id}] connected`);
-});
+aedes.on('client', (client) => {
+	console.log(`Client [${client.id}] connected`)
+})
 
-aedes.on('clientDisconnect', client => {
-  console.log(`Client [${client.id}] disconnected`);
-});
+aedes.on('clientDisconnect', (client) => {
+	console.log(`Client [${client.id}] disconnected`)
+})
 
 aedes.on('clientError', (client, err) => {
-  console.log(`Client [${client.id}] encountered error: ${JSON.stringify(err)}`);
-});
+	console.log(`Client [${client.id}] encountered error: ${JSON.stringify(err)}`)
+})
 
 aedes.on('publish', (packet, client) => {
-  client ? console.log(`Client [${client.id}] published on ${packet.topic}: ${packet.payload}`)
-    : console.log(`aedes published on ${packet.topic}: ${packet.payload}`);
-});
+	client
+		? console.log(
+				`Client [${client.id}] published on ${packet.topic}: ${packet.payload}`
+		  )
+		: console.log(`aedes published on ${packet.topic}: ${packet.payload}`)
+})
 
 aedes.on('subscribe', (subscriptions, client) => {
-  var subscriptionArr = subscriptions.map(subscription => {
-    return `${subscription['topic']} (${subscription['qos']})`;
-  });
-  client ? console.log(`Client [${client.id}] subscribed ${subscriptionArr}`)
-    : console.log(`aedes subscribed ${subscriptionArr.topic}: ${subscriptionArr.payload}`);
-});
+	var subscriptionArr = subscriptions.map((subscription) => {
+		return `${subscription['topic']} (${subscription['qos']})`
+	})
+	client
+		? console.log(`Client [${client.id}] subscribed ${subscriptionArr}`)
+		: console.log(
+				`aedes subscribed ${subscriptionArr.topic}: ${subscriptionArr.payload}`
+		  )
+})
 
 aedes.on('unsubscribe', (unsubscriptions, client) => {
-  console.log(`Client [${client.id}] unsubscribe ${unsubscriptions}`);
-});
+	console.log(`Client [${client.id}] unsubscribe ${unsubscriptions}`)
+})
 
 aedes.on('ack', function (message, client) {
-  console.log('%s ack\'d message', client.id);
-});
+	console.log("%s ack'd message", client.id)
+})
 
 // aedes.on('subscribe', function (subscriptions, client) {
 //   if (client) {
@@ -94,5 +88,5 @@ aedes.on('ack', function (message, client) {
 // });
 
 aedes.on('clientDisconnect', function (client) {
-  console.log('%s disconnected', client.id);
-});
+	console.log('%s disconnected', client.id)
+})
--- redisConfig.js
+++ redisConfig.js
@@ -1,11 +1,12 @@
 let config = {
-  mqtt_port: 2883,
-  ws_port: 8880,
-  redis_port: 6379,
-  redis_host: '192.168.0.101',
-  redis_pass: 'carson',
-  redis_dbA: 0,
-  redis_dbB: 1
-};
+	mqtt_port: 1883,
+	ws_port: 8880,
+	redis_port: 6379,
+	// redis_host: '172.20.10.3',
+	redis_host: '127.0.0.1',
+	redis_pass: 'carson',
+	redis_dbA: 0,
+	redis_dbB: 1,
+}
 
-module.exports = config;
+module.exports = config
